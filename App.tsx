eb7b50e82f58f46e4ae3f20e4a8faa8baad5aac0
oterte2891@gmail.com <oterte2891@gmail.com>
2025-06-19 15:58:30 +0900

    수정사항

--- App.tsx
+++ App.tsx
@@ -19,7 +19,6 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    
     <NavigationContainer>
       {/* headerShown: false는 상단에 기본 헤더(뒤로가기 버튼 등)를 숨기는 역할 */}
       <Stack.Navigator screenOptions={{ headerShown: false }}>
