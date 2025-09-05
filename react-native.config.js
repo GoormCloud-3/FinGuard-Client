4d704001ba69696ef8f6920150ef886be0437866
oterte2891@gmail.com <oterte2891@gmail.com>
2025-09-05 16:17:07 +0900

    수정내용 다수 추가/ 아직 해결 안됨

--- react-native.config.js
+++ react-native.config.js
@@ -6,5 +6,8 @@ module.exports = {
     // amazon-cognito-identity-js는 JS 라이브러리라 네이티브 불필요.
     // rnconfig에 네이티브 섹션이 잡혀 충돌하므로 오토링킹만 오프.
     'amazon-cognito-identity-js': { platforms: { android: null, ios: null } },
+    '@notifee/react-native': {
+      platforms: { android: null },
+    },
   },
 };
